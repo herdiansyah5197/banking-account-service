f2d90373cc79ecfcd8aefaec1bff6a336f7232ea
ians herdiansyah <herdiansyah1797@gmail.com>
2026-01-25 10:10:06 +0700

    feat : redisManage - setup redis

--- src/main/java/id/banking/accountservice/config/RedisCacheConfig.java
+++ src/main/java/id/banking/accountservice/config/RedisCacheConfig.java
@@ -2,6 +2,7 @@ package id.banking.accountservice.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -10,6 +11,7 @@ import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
+@Configuration
 public class RedisCacheConfig {
 
 
